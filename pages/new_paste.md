390b16934a37f2b7ab9dff950257f30778bb139f
Jacob Mastel <jacobm001@gmail.com>
2017-05-22 11:34:27 -0700

    started new-paste form

---
+++ pages/new_paste.md
@@ -0,0 +1,18 @@
+---
+title: New Paste
+# template: default
+
+form:
+  name: pastebin-new
+  # action
+  method: post
+
+  fields:
+    - name: title
+      tye: text
+      id: title
+      autofocus: true
+
+---
+
+# Hello, World
--- pastebin.php
+++ pastebin.php
@@ -1,6 +1,8 @@
 <?php
 namespace Grav\Plugin;
 
+use Grav\Common\Page\Page;
+use Grav\Common\Page\Pages;
 use Grav\Common\Plugin;
 use RocketTheme\Toolbox\Event\Event;
 
@@ -23,7 +25,9 @@ class PastebinPlugin extends Plugin
     public static function getSubscribedEvents()
     {
         return [
-            'onPluginsInitialized' => ['onPluginsInitialized', 0]
+            'onPluginsInitialized' => ['onPluginsInitialized', 0],
+            'onTwigTemplatePaths'  => ['onTwigTemplatePaths', 0],
+            'onFormProcessed'      => ['onFormProcessed', 0]
         ];
     }
 
@@ -54,9 +58,33 @@ class PastebinPlugin extends Plugin
         // ]);
     }
 
+    public function onTwigTemplatePaths()
+    {
+        $twig = $this->grav['twig'];
+        $twig->twig_paths[] = __DIR__ . '/templates';
+    }
+
     public function addNewPastePage() 
     {
-        // $pages = $this->
-        $this->grav['debugger']->addMessage('BAM!');
+        $this->grav['debugger']->addMessage('Building new paste page');
+
+        $route = $this->config->get('plugins.pastebin.route_new');
+        $pages = $this->grav['pages'];
+        $page = $pages->dispatch($route);
+
+        if (!$page) {
+            $page = new Page;
+            $page->init(new \SplFileInfo(__DIR__ . "/pages/new_paste.md"));
+            $page->slug(basename($route));
+
+            $pages->addPage($page, $route);
+            unset($this->grav['page']);
+            $this->grav['page'] = $page;
+        }
+    }
+
+    public function onFormProcessed(Event $event)
+    {
+        $this->grav['debugger']->addMessage('Processing form!');
     }
 }
